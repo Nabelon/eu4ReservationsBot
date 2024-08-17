a5154dbcc047fac26ed5dd354549520973453e69
K. <htx.jcr@gmail.com>
2024-08-16 20:45:46 -0500

    Working Test

--- main.py
+++ main.py
@@ -104,123 +104,165 @@ valid_gamemodes = config['gamemodes']
 
 async def sendHelpMessage(channel):
     embed = discord.Embed(title="List of Commands", color=discord.Color.blue())
-    embed.add_field(name="!start [gamemode]", value=f"Starts the reservation process for the specified gamemode. You must use this command before any reservations can be made.\nAvailable gamemodes: {', '.join(valid_gamemodes)}", inline=False)
-    embed.add_field(name="!reserve [nation]", value="Reserves a nation for yourself. Make sure to start the reservation process with !start before using this command.", inline=False)
-    embed.add_field(name="!reserve [nation] [user]", value="Allows game managers (those with mute permissions) to reserve a nation for another user.", inline=False)
-    embed.add_field(name="!unreserve", value="Removes your current reservation. Game managers can also use this command to unreserve a nation for another user by mentioning them.", inline=False)
-    embed.add_field(name="!delete", value="Deletes all reservations and the logged gamemode for the current channel. Only administrators can use this command.", inline=False)
-    embed.add_field(name="!help", value="Displays this list of commands.", inline=False)
+    embed.add_field(name="`!start [gamemode]`", value=f"Starts the reservation process for the specified gamemode. You must use this command before any reservations can be made.\nAvailable gamemodes: {', '.join(valid_gamemodes)}", inline=False)
+    embed.add_field(name="`!reserve [nation]`", value="Reserves a nation for yourself. Make sure to start the reservation process with !start before using this command.", inline=False)
+    embed.add_field(name="`!reserve [nation] [user]`", value="Allows game managers (those with mute permissions) to reserve a nation for another user.", inline=False)
+    embed.add_field(name="`!unreserve`", value="Removes your current reservation. Game managers can also use this command to unreserve a nation for another user by mentioning them.", inline=False)
+    embed.add_field(name="`!delete`", value="Deletes all reservations and the logged gamemode for the current channel. Only administrators can use this command.", inline=False)
+    embed.add_field(name="`!help`", value="Displays this list of commands.", inline=False)
     embed.set_footer(text="Use these commands to manage your game's reservations.")
     await channel.send(embed=embed)
+
+async def delete_after_delay(message, delay=5):
+    await asyncio.sleep(delay)
+    await message.delete()
 
 @client.event
 async def on_message(message):
     if message.author != client.user and message.guild:
-        c.execute('SELECT * FROM reservation_logs WHERE channel_id = ?', (message.channel.id,))
-        log = c.fetchone()
-        if not log:
-            if message.content.startswith("!start"):
-                parts = message.content.split()
-                gamemode = parts[1].lower() if len(parts) > 1 else config["default_gamemode"]
+        try:
+            # Connect to the database
+            conn = sqlite3.connect('reservations.db')
+            c = conn.cursor()
 
-                if gamemode not in valid_gamemodes:
-                    await message.channel.send(f"Error: Gamemode '{gamemode}' is not recognized. Please choose from the following: {', '.join(valid_gamemodes)}.")
-                    await message.delete()
-                    return
+            # Fetch the reservation log for the channel
+            c.execute('SELECT * FROM reservation_logs WHERE channel_id = ?', (message.channel.id,))
+            log = c.fetchone()
 
-                try:
-                    print(f"Starting reservations in channel {message.channel.name} for gamemode: {gamemode}")
-
-                    c.execute('INSERT INTO reservation_logs (channel_id, gamemode, creation_date) VALUES (?, ?, ?)',
-                              (message.channel.id, gamemode, time.strftime('%Y-%m-%d %H:%M:%S')))
-                    conn.commit()
-
-                    await message.delete()
-
-                    await updateMap(message, message.channel, message.channel, gamemode=gamemode)
-
-                    return
-                except Exception as e:
-                    print(f"Error in !start command: {e}")
-                    await message.author.send("An error occurred while starting the reservation. Please try again.")
-                return
-
-            if message.content.startswith("!reserve") or message.content.startswith("!r "):
-                await message.channel.send("Please use the !start command to initiate reservations before reserving a nation.")
-                await message.delete()
-            return
-        if log:
-            gamemode = log[1]
-            if message.content.startswith("!reserve") or message.content.startswith("!r "):
-                try:
+            if not log:
+                if message.content.startswith("!start"):
                     parts = message.content.split()
-                    if len(parts) < 2:
-                        await message.channel.send("You must specify a nation to reserve.")
+                    gamemode = parts[1].lower() if len(parts) > 1 else config["default_gamemode"]
+
+                    if gamemode not in valid_gamemodes:
+                        await message.author.send(f"Error: Gamemode '{gamemode}' is not recognized. Please choose from the following: {', '.join(valid_gamemodes)}.")
+                        await delete_after_delay(message)
                         return
-                    nation = parts[1].lower()
-                    if len(message.mentions) > 0 and message.author.guild_permissions.mute_members:
-                        user_id = message.mentions[0].id
-                    else:
-                        user_id = message.author.id
-                    if nation in nationsToTag:
-                        c.execute('SELECT nation FROM reservations WHERE channel_id = ? AND user_id = ?', (message.channel.id, user_id))
-                        old_reservation = c.fetchone()
-                        if old_reservation:
-                            c.execute('DELETE FROM reservations WHERE channel_id = ? AND user_id = ?', (message.channel.id, user_id))
-                            print(f"User <@{user_id}> switched from {old_reservation[0]} to {nation}.")
-                        c.execute('INSERT INTO reservations (server_id, channel_id, user_id, nation, date) VALUES (?, ?, ?, ?, ?)',
-                                  (message.guild.id, message.channel.id, user_id, nation, time.strftime('%Y-%m-%d %H:%M:%S')))
+
+                    try:
+                        print(f"Starting reservations in channel {message.channel.name} for gamemode: {gamemode}")
+
+                        c.execute('INSERT INTO reservation_logs (channel_id, gamemode, creation_date) VALUES (?, ?, ?)',
+                                  (message.channel.id, gamemode, time.strftime('%Y-%m-%d %H:%M:%S')))
                         conn.commit()
 
-                        await message.delete()
+                        await delete_after_delay(message)
 
                         await updateMap(message, message.channel, message.channel, gamemode=gamemode)
-                    else:
-                        await message.channel.send(f"Nation '{nation}' not recognized.")
-                except Exception as e:
-                    await message.channel.send(f"Error: \n{str(e)}")
-            return
 
-        if message.content.startswith("!unreserve"):
-            try:
-                user_id = message.author.id
-                if len(message.mentions) > 0:
-                    if message.author.guild_permissions.mute_members:
-                        user_id = message.mentions[0].id
-                    else:
-                        await message.channel.send("You don't have permission to unreserve for others.")
                         return
-                
-                c.execute('DELETE FROM reservations WHERE channel_id = ? AND user_id = ?', (message.channel.id, user_id))
-                conn.commit()
+                    except Exception as e:
+                        print(f"Error in !start command: {e}")
+                        await message.author.send("An error occurred while starting the reservation. Please try again.")
+                    return
 
-                await message.delete()
+                if message.content.startswith("!reserve") or message.content.startswith("!r "):
+                    await message.author.send("Please use the !start command to initiate reservations before reserving a nation.")
+                    await delete_after_delay(message)
+                return
 
-                if log:
-                    gamemode = log[1]
-                    await updateMap(message, message.channel, message.channel, gamemode=gamemode)
+            if log:
+                gamemode = log[1]
 
-            except Exception as e:
-                await message.channel.send(f"Error: \n{str(e)}")
-            return
+                if message.content.startswith("!reserve") or message.content.startswith("!r "):
+                    try:
+                        parts = message.content.split()
+                        if len(parts) < 2:
+                            await message.author.send("You must specify a nation to reserve.")
+                            await delete_after_delay(message)
+                            return
 
-        if message.content.startswith("!delete"):
-            if message.author.guild_permissions.administrator:
-                try:
-                    c.execute('DELETE FROM reservations WHERE channel_id = ?', (message.channel.id,))
-                    c.execute('DELETE FROM reservation_logs WHERE channel_id = ?', (message.channel.id,))
-                    conn.commit()
+                        nation = parts[1].lower()
+                        if len(message.mentions) > 0 and message.author.guild_permissions.mute_members:
+                            user_id = message.mentions[0].id
+                        else:
+                            user_id = message.author.id
 
-                    confirmation_message = await message.channel.send("All reservations and logs have been deleted for this channel.")
+                        if nation in nationsToTag:
+                            c.execute('SELECT nation FROM reservations WHERE channel_id = ? AND user_id = ?', (message.channel.id, user_id))
+                            old_reservation = c.fetchone()
+                            if old_reservation:
+                                c.execute('DELETE FROM reservations WHERE channel_id = ? AND user_id = ?', (message.channel.id, user_id))
+                                print(f"User <@{user_id}> switched from {old_reservation[0]} to {nation}.")
+                            c.execute('INSERT INTO reservations (server_id, channel_id, user_id, nation, date) VALUES (?, ?, ?, ?, ?)',
+                                      (message.guild.id, message.channel.id, user_id, nation, time.strftime('%Y-%m-%d %H:%M:%S')))
+                            conn.commit()
 
-                    await message.delete()
-                    await confirmation_message.delete()
+                            await delete_after_delay(message)
 
-                except Exception as e:
-                    await message.channel.send(f"Error: \n{str(e)}")
-            else:
-                await message.channel.send("You do not have permission to use this command.")
-            return
+                            await updateMap(message, message.channel, message.channel, gamemode=gamemode)
+                        else:
+                            await message.author.send(f"Nation '{nation}' not recognized.")
+                            await delete_after_delay(message)
+                    except Exception as e:
+                        await message.author.send(f"Error: \n{str(e)}")
+                        await delete_after_delay(message)
+                        print(f"Error in !reserve command: {e}")
+                    return
+
+                if message.content.startswith("!unreserve"):
+                    try:
+                        user_id = message.author.id
+                        if len(message.mentions) > 0:
+                            if message.author.guild_permissions.mute_members:
+                                user_id = message.mentions[0].id
+                            else:
+                                await message.author.send("You don't have permission to unreserve for others.")
+                                await delete_after_delay(message)
+                                return
+
+                        c.execute('DELETE FROM reservations WHERE channel_id = ? AND user_id = ?', (message.channel.id, user_id))
+                        conn.commit()
+
+                        if log:
+                            gamemode = log[1]
+                            await updateMap(message, message.channel, message.channel, gamemode=gamemode)
+
+                        await delete_after_delay(message)
+                        print(f"User <@{user_id}> unreserved successfully in channel {message.channel.name}.")
+
+                    except Exception as e:
+                        await message.author.send(f"Error: \n{str(e)}")
+                        await delete_after_delay(message)
+                        print(f"Error in !unreserve command: {e}")
+                    return
+
+                if message.content.startswith("!delete"):
+                    if message.author.guild_permissions.administrator:
+                        try:
+                            c.execute('DELETE FROM reservations WHERE channel_id = ?', (message.channel.id,))
+                            c.execute('DELETE FROM reservation_logs WHERE channel_id = ?', (message.channel.id,))
+                            conn.commit()
+
+                            confirmation_message = await message.author.send("All reservations and logs have been deleted for this channel.")
+                            
+                            await delete_after_delay(message)
+                            await delete_after_delay(confirmation_message, delay=10)
+
+                            print(f"All reservations and logs deleted for channel {message.channel.name}.")
+
+                        except Exception as e:
+                            await message.author.send(f"Error: \n{str(e)}")
+                            await delete_after_delay(message)
+                            print(f"Error in !delete command: {e}")
+                    else:
+                        await message.author.send("You do not have permission to use this command.")
+                        await delete_after_delay(message)
+                    return
+
+            # Ensure all other messages (commands) are deleted after processing
+            await client.process_commands(message)
+            await delete_after_delay(message)
+
+        except sqlite3.Error as db_error:
+            print(f"Database error: {db_error}")
+            await message.author.send("An error occurred while accessing the database. Please contact the administrator.")
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            await message.author.send("An unexpected error occurred. Please try again.")
+        finally:
+            # Ensure the database connection is closed after processing
+            conn.close()
 
 async def delete_messages_after_start(channel):
     async for message in channel.history(limit=200):
